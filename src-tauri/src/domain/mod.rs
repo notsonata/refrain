515b8ed19176a6905997a9ba68adf0d090145a98
Angelo David <90495108+notsonata@users.noreply.github.com>
2026-09-23 23:11:48 +0800

    chore: format milestone 5 Rust

--- src-tauri/src/domain/mod.rs
+++ src-tauri/src/domain/mod.rs
@@ -3,7 +3,8 @@ mod source;
 
 pub use settings::AppSettings;
 pub use source::{
-    CollectionEntry, SourceAccount, SourceAccountOverview, SourceCollection, SourceCollectionEntryView,
-    SourceCollectionItem, SourceCollectionListPage, SourceCollectionPage, SourceCollectionSummary,
-    SourceTrack, SourceTrackView, SpotifySourceOverview,
+    CollectionEntry, SourceAccount, SourceAccountOverview, SourceCollection,
+    SourceCollectionEntryView, SourceCollectionItem, SourceCollectionListPage,
+    SourceCollectionPage, SourceCollectionSummary, SourceTrack, SourceTrackView,
+    SpotifySourceOverview,
 };
